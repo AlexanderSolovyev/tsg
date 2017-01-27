fae20d80618a6f4662df4476b61e068cbf49bca9
AlexanderSolovyev <1430@list.ru>
2017-01-27 17:18:44 +0300

    written update for news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -5,4 +5,14 @@ class NewsController < ApplicationController
   def edit 
     @new=New.find(params[:id])
   end
+  def update 
+    new= New.find(params[:id])
+    new.update!(new_params)
+    redirect_to news_index_path
+  end
+
+   private
+   def new_params
+     params.require(:new).permit(:title, :description)
+   end
 end
