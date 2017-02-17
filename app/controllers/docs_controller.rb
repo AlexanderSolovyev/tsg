98f26a9c767ace9a7adf07e845faa21aff143c44
AlexanderSolovyev <1430@list.ru>
2017-02-18 00:28:54 +0300

    fix bug in docs controller

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,5 +1,6 @@
 class DocsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :auth_admin!, except: [:index]
   before_action :doc_find, only: [:edit, :update, :destroy]
   def index
     @docs=Doc.all
@@ -28,4 +29,7 @@ class DocsController < ApplicationController
   def doc_find
     @doc=Doc.find(params[:id])
   end
+   def auth_admin!
+      redirect_to news_index_path unless user_signed_in? and current_user.admin 
+   end
 end
